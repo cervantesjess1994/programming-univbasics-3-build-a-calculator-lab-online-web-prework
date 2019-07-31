ec5ed005bd437134a1bf3e8876c59785180a41ab
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-07-31 11:28:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
-def addition(num1, num2)
-
+def addition(num1+num2)
+ puts "5 + 4"
+ puts addition
 end
 
 def subtraction(num1, num2)
-
+  puts "#{num1} - #{num2}"
+  puts subtraction
 end
 
 def division(num1, num2)
-
+  put "#{num1} / #{num2}"
+  puts division
 end
 
 def multiplication(num1, num2)
-
+  puts "#{num1} * #{num2}"
+  puts multiplication
 end
 
 def modulo(num1, num2)
-
+  puts "#{num1} % #{num2}"
+  puts modulo
 end
 
 def square_root(num)
-
+  puts "Math.sqrt(#{num})"
+  puts square_root
 end
